36b2d10d605c6d961cdf4eaf043dfbf80c60a511
Hezron Sison <99523830+hinokunn1@users.noreply.github.com>
2026-06-05 10:33:11 +0800

    Create register.js

---
+++ register.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBAdWVto0vUd3Wuw_lLLQUrHqizo_2h3Dk",
+    authDomain: "attendify-90a4f.firebaseapp.com",
+    projectId: "attendify-90a4f",
+    storageBucket: "attendify-90a4f.firebasestorage.app",
+    messagingSenderId: "29544428843",
+    appId: "1:29544428843:web:b9541410d1c862d3e0016e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
